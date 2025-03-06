8ab22871a067563972fc8f88e42f753e49aaf182
Itoromfon <itoromfon746@gmail.com>
2025-03-07 00:42:33 +0100

    made some change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,6 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import Navbar from "@/components/navbar/Navbar";
 import Hero from "@/components/hero/Hero";
 import Features from "@/components/features/Features";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export default function Home() {
   return (
